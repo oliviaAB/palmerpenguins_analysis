5ff1abebf0caeb393f5fe9386b5bc08c9b66f168
Olivia Angelin-Bonnet <Olivia.Angelin-Bonnet@plantandfood.co.nz>
2024-07-18 21:36:20 +1200

    Turned first code into functions

---
+++ R/helper_functions.R
@@ -0,0 +1,75 @@
+#' Read and clean data
+#' 
+#' Reads in the penguins data, renames and select relevant columns. The
+#' following transformations are applied to the data: 
+#' * only keep species common name
+#' * extract observation year
+#' * remove rows with missing values
+#' 
+#' @param file Character, path to the penguins data .csv file.
+#' @returns A tibble.
+read_data <- function(file) {
+  readr::read_csv(file, show_col_types = FALSE) |> 
+    janitor::clean_names() |> 
+    dplyr::mutate(
+      species = stringr::word(species, 1),
+      year = lubridate::year(date_egg),
+      sex = stringr::str_to_lower(sex),
+      year = as.integer(year),
+      body_mass_g = as.integer(body_mass_g),
+      dplyr::across(dplyr::where(is.character), as.factor)
+    ) |> 
+    dplyr::select(
+      species,
+      island,
+      year,
+      sex,
+      body_mass_g,
+      bill_length_mm = culmen_length_mm,
+      bill_depth_mm = culmen_depth_mm,
+      flipper_length_mm
+    ) |> 
+    tidyr::drop_na()
+}
+
+#' Violin plot of variable per island and sex
+#'
+#' Visualise using violin plots the distribution of a categorical variable, with
+#' island on the x-axis and sex as colour.
+#'
+#' @param df Tibble, penguins data.
+#' @param yvar Unquoted expression, name of the variable in `df` to plot.
+#' @returns a ggplot.
+violin_plot <- function(df, yvar) {
+  df |> 
+    ggplot2::ggplot(
+      ggplot2::aes(x = island, colour = sex, fill = sex, y = {{ yvar }})
+    ) +
+    ggplot2::geom_violin(alpha = 0.3, scale = "width") +
+    ggbeeswarm::geom_quasirandom(dodge.width = 0.9) +
+    ggplot2::scale_colour_brewer(palette = "Set1") +
+    ggplot2::scale_fill_brewer(palette = "Set1") +
+    ggplot2::theme_minimal()
+}
+
+#' Scatterplot of bill length vs depth
+#'
+#' Plots bill length (x-axis) against bill depth (y-axis), with island as colour
+#' and sex as point shape.
+#'
+#' @param df Tibble, penguins data.
+#' @returns a ggplot.
+plot_bill_length_depth <- function(df) {
+  df |> 
+    ggplot2::ggplot(
+      ggplot2::aes(
+        x = bill_length_mm, 
+        y = bill_depth_mm, 
+        colour = island, 
+        shape = sex
+        )
+    ) +
+    ggplot2::geom_point() +
+    ggplot2::scale_colour_brewer(palette = "Set2") +
+    ggplot2::theme_minimal()
+}
--- analysis/first_script.R
+++ analysis/first_script.R
@@ -1,52 +1,11 @@
-library(tidyverse)
-library(janitor)
-library(ggbeeswarm)
 library(here)
 
-# Reading and cleaning data
-penguins_df <- read_csv(here("data/penguins_raw.csv"), show_col_types = FALSE) |> 
-  clean_names() |> 
-  mutate(
-    species = word(species, 1),
-    year = year(date_egg),
-    sex = str_to_lower(sex),
-    year = as.integer(year),
-    body_mass_g = as.integer(body_mass_g),
-    across(where(is.character), as.factor)
-  ) |> 
-  select(
-    species,
-    island,
-    year,
-    sex,
-    body_mass_g,
-    bill_length_mm = culmen_length_mm,
-    bill_depth_mm = culmen_depth_mm,
-    flipper_length_mm
-  ) |> 
-  drop_na()
+source(here("R/helper_functions.R"))
 
-## Violin plot of body mass per island and sex
-penguins_df |> 
-  ggplot(aes(x = island, colour = sex, fill = sex, y = body_mass_g)) +
-  geom_violin(alpha = 0.3, scale = "width") +
-  geom_quasirandom(dodge.width = 0.9) +
-  scale_colour_brewer(palette = "Set1") +
-  scale_fill_brewer(palette = "Set1") +
-  theme_minimal()
+penguins_df <- read_data(here("data/penguins_raw.csv"))
 
-## Violin plot of flipper length per island and sex
-penguins_df |> 
-  ggplot(aes(x = island, colour = sex, fill = sex, y = flipper_length_mm)) +
-  geom_violin(alpha = 0.3, scale = "width") +
-  geom_quasirandom(dodge.width = 0.9) +
-  scale_colour_brewer(palette = "Set1") +
-  scale_fill_brewer(palette = "Set1") +
-  theme_minimal()
+body_mass_plot <- violin_plot(penguins_df, body_mass_g)
 
-## Scatter plot of bill length vs depth, coloured by island and 
-penguins_df |> 
-  ggplot(aes(x = bill_length_mm, y = bill_depth_mm, colour = island, shape = sex)) +
-  geom_point() +
-  scale_colour_brewer(palette = "Set2") +
-  theme_minimal()
+flipper_length_plot <- violin_plot(penguins_df, flipper_length_mm)
+
+bill_scatterplot <- plot_bill_length_depth(penguins_df)
